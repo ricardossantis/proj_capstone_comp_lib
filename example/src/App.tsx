7acef18d453600cf6fa55afe25685d588116d28d
Ricardo <rsdsantis@gmail.com>
2020-09-22 18:08:35 -0300

    Feat: add modal with styled components

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-
-import { ExampleComponent } from 'proj-capstone-lib'
+import React, { useState } from 'react'
+import { Modal } from 'proj-capstone-lib'
 import 'proj-capstone-lib/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [visible, setVisibility] = useState(false)
+
+  return (
+    <div>
+      <Modal visible={visible} title='Modal'>
+        <input placeholder='teste' />
+        <button>send</button>
+      </Modal>
+      <button onClick={() => setVisibility(!visible)}>Open Modal</button>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/modal/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface modalProps {
+  visible: boolean
+  title: string
+  children: React.ReactNode
+}
+
+const Modal = (props: modalProps) => {
+  return props.visible ? (
+    <ModalDiv>
+      <ModalHeader>
+        <ModalTextBox>{props.title}</ModalTextBox>
+        <ModalButton>X</ModalButton>
+      </ModalHeader>
+      <ModalBody>{props.children}</ModalBody>
+    </ModalDiv>
+  ) : null
+}
+
+export default Modal
+
+const ModalDiv = styled.div`
+  width: 30%;
+  min-height: 500px;
+  background-color: #333;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const ModalHeader = styled.div`
+  width: 100%;
+  background-color: #000;
+  height: 50px;
+  align-self: flex-start;
+  display: flex;
+  align-items: center;
+  color: white;
+`
+
+const ModalButton = styled.button`
+  background-color: #777;
+  color: #000;
+  border-radius: 50%;
+  width: fit-content;
+  height: fit-content;
+  margin: calc(100% - width);
+`
+const ModalTextBox = styled.div`
+  width: fit-content;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`
+const ModalBody = styled.div`
+  width: 100%;
+  min-height: 450px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+`
